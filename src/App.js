f302ef1d6a279b652dc9fd6d1ddf5ef4c74d2b7d
Addis Mekuria <addismekuria@Addiss-MacBook-Pro.local>
2022-06-30 16:00:16 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Wanna get recognized on the market?
+        </p>
+        
+          Lets Hunt some Jobs!!!
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
