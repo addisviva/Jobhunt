6bf460780ca1527a2e1ef8ffee6aed6940fab46c
Addis Mekuria <addismekuria@Addiss-MacBook-Pro.local>
2022-07-06 12:31:47 -0700

    commit styled components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import Landing from "./pages/Landing";
-import styled from "styled-components";
 
-// const Button = styled.button`
-//   background: red;
-//   color:white ;
-//   font-size:1rem ;
 
-// `
+
+
 function App() {
   return (
     <div className="App">
-      {/* <Button>click me</Button> */}
+      
           <h1> jobify</h1>
           <Landing />
     </div>
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,29 +1,34 @@
-import React from 'react'
-import logo from './../assets/images/logo.svg'
-import main from './../assets/images/main.svg'
-
+import React from "react";
+import logo from "./../assets/images/logo.svg";
+import main from "./../assets/images/main-alternative.svg";
+import Wrapper from "../assets/wrappers/Testing";
 function Landing() {
-    return (
-        <main>
-            <nav>
-                <img src={logo} alt="jobify" className='logo' />
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span>
+          </h1>
+          App
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+            molestiae? Eius ipsa incidunt, ea modi culpa impedit recusandae non
+            corrupti veritatis tempore laborum nesciunt perspiciatis obcaecati
+            distinctio aliquam corporis dolor.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
 
-            </nav>
-            <div className='container page'>
-                <div className="info">
-                    <h1>job <span>tracking</span></h1>App
-                    <p>Lorem ipsum dolor sit amet consectetur 
-                        adipisicing elit. Quos, molestiae? Eius ipsa
-                         incidunt, ea modi culpa impedit recusandae non
-                          corrupti veritatis tempore laborum nesciunt perspiciatis
-                         obcaecati distinctio aliquam corporis dolor.
-                         </p>
-                         <button className='btn btn-hero'>Login/Register</button>
-                </div>
-            </div>
-            <img src={main} alt="job hunt" className='img main-img' />
-        </main>
-    )
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
 }
 
-export default Landing
+
+
+export default Landing;
